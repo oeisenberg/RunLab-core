53c3a07970787570d797c15a471dfdb8c0c40fd8
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-15 19:53:58 +0100

    OE - expanding activity obj to support more

--- runlab/src/main/java/RunLab/Objects/Activity.java
+++ runlab/src/main/java/RunLab/Objects/Activity.java
@@ -1,30 +1,30 @@
 package RunLab.Objects;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.Map.Entry;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import RunLab.Utility.JsonConverter;
 
 /**
  * class description
  *
  */
 public class Activity {
+    private String[] vars = {"athlete_id", "name", "distance", "moving_time", "elapsed_time", "total_elevation_gain", "start_date",
+                            "start_latlng", "end_latlng", "map", "average_speed", "max_speed", "has_heartrate", "average_heartrate", 
+                            "max_heartrate", "elev_high", "elev_low", "calories", "perceived_exertion", "splits", "laps", "type"};
     private Integer athlete_id;
-    private float name;
-    private double distance;
+    private String name;
+    private float distance;
     private float moving_time;
     private float elapsed_time;
     private float total_elevation_gain;
     private float start_date;
     private float start_latlng; // create latlong object
     private float end_latlng;
-    private float map; // create map object (id, ployline/summary polyline)
+    private Route route; // create route object (id, ployline/summary polyline)
     private float average_speed;
     private float max_speed;
     private float has_heartrate;
@@ -37,25 +37,30 @@ public class Activity {
     private float splits; // custom object split
     private float laps;
     private float type;
-
-    private static Map<String, Object> attributes;
-
+    
     public Activity(String jsonAsString) {
         JsonObject jsonObject = (JsonObject) JsonParser.parseString(jsonAsString);
         
-        attributes = new HashMap<String, Object>();
-        Set<Entry<String, JsonElement>> entrySet = jsonObject.entrySet();
-        for(Map.Entry<String,JsonElement> entry : entrySet){
-          attributes.put(entry.getKey(), jsonObject.get(entry.getKey()));
-        }
+        Map<String, Object> attributes = JsonConverter.toMap(jsonObject);
+        
+        // athlete_id = JsonConverter.toInt(attributes, vars[0]); // need to extract the second layer
+        name = JsonConverter.toString(attributes, vars[1]);
+        distance = JsonConverter.toFloat(attributes, vars[2]);
+        moving_time =  JsonConverter.toFloat(attributes, vars[3]);
+        elapsed_time =  JsonConverter.toFloat(attributes, vars[4]);
+        total_elevation_gain =  JsonConverter.toFloat(attributes, vars[5]);
+        // start_date =  JsonConverter.toFloat(attributes, vars[6]); // need to change to date time 
+        // start_latlng =  JsonConverter.toFloat(attributes, vars[7]); // need to change to latlng
+        // end_latlng =  JsonConverter.toFloat(attributes, vars[8]); // need to change to latlng
+        route = JsonConverter.toRoute(attributes, vars[9]);
     }
     
-    public String getMap(){
-        JsonObject map = (JsonObject) attributes.get("map");
+    // public String getMap(){
+    //     JsonObject map = (JsonObject) attributes.get("map");
 
-        String polyline = map.get("polyline").getAsString();
+    //     String polyline = map.get("polyline").getAsString();
         
-        return polyline; // Let JS decode polyline, Maps JavaScript API in google.maps.geometry.encoding namespace
-    }
+    //     return polyline; // Let JS decode polyline, Maps JavaScript API in google.maps.geometry.encoding namespace
+    // }
 
 }

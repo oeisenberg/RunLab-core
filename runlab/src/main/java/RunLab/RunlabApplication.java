1a8d93ab3c9aed96ea9e990ebf56571856bf181e
Oliver <oliver.eisenberg88@gmail.com>
2022-06-24 18:17:51 +0100

    Refactoring and adding new endpoint

--- runlab/src/main/java/RunLab/RunlabApplication.java
+++ runlab/src/main/java/RunLab/RunlabApplication.java
@@ -1,12 +1,16 @@
 package RunLab;
 
+import java.net.http.HttpResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import RunLab.Exceptions.InvalidRequest;
 import RunLab.Models.*;
 import RunLab.Responces.*;
 import RunLab.Wrappers.*;
@@ -18,31 +22,52 @@ public class RunlabApplication {
 
     Strava stravaWrapper = new Strava();
     Mapbox mapboxWrapper = new Mapbox("walking");
-    MongoDB dbWrappers = new MongoDB();
+    // MongoDB dbWrappers = new MongoDB();
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.setProperty("server.servlet.context-path", "/Runlab");
-		SpringApplication.run(RunlabApplication.class, args);
-	}
+        SpringApplication.run(RunlabApplication.class, args);
+    }
 
     @GetMapping("/refresh")
-    public Responce refresh(){
+    public Response refresh() {
         // pull in and update data
         boolean dataUpdated = this.stravaWrapper.pull();
 
-        if(dataUpdated){
+        if (dataUpdated) {
             return new Success();
         } else {
             return new PullFailure();
         }
     }
 
+    @GetMapping("/getAtheleteProfile")
+    public void getAtheleteProfile() {
+        // TODO: Query to get athelete information.
+    }
+
+    @GetMapping(value = { "/getAtheleteStatistics/{atheleteID}" })
+    public Response getAtheleteStatistics(@PathVariable(value = "atheleteID") String atheleteID) {
+        try {
+            HttpResponse<String> response = this.stravaWrapper.getAtheleteStats(atheleteID);
+            if (response.statusCode() == 200){
+                Response myResponse = new Success();
+                myResponse.ingest(response);
+                return myResponse;
+            }
+        } catch (InvalidRequest e) {
+            return new Failure();
+        }
+
+        return new Failure();
+    }
+
     @GetMapping("/oauth")
-    public Responce oauthGET(){
+    public Response oauthGET() {
         // pull in and update data
         boolean dataUpdated = this.stravaWrapper.refreshAuthTokens();
 
-        if(dataUpdated){
+        if (dataUpdated) {
             return new Success();
         } else {
             return new PullFailure();
@@ -50,14 +75,14 @@ public class RunlabApplication {
     }
 
     @PostMapping("/oauth")
-    public Responce oauthPOST(@RequestBody codeModel responceBody){
+    public Response oauthPOST(@RequestBody codeModel responceBody) {
         Boolean dataUpdated = this.stravaWrapper.setAuthTokens(responceBody);
 
-        if(dataUpdated){
+        if (dataUpdated) {
             return new Success();
         } else {
             return new Failure();
         }
     }
-    
+
 }

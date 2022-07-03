e09b601f33974396d8e510ac190f85939658cab9
Oliver <oliver.eisenberg88@gmail.com>
2022-07-03 15:23:36 +0100

    Created abstract class for Athlete stats class

---
+++ runlab/src/main/java/RunLab/Objects/Strava/ActivityTotal.java
@@ -0,0 +1,10 @@
+package RunLab.Objects.Strava;
+
+public abstract class ActivityTotal {
+    private int count;
+    private float distnace;
+    private int moving_time;
+    private int elapsed_time;
+    private float elevation_gain;
+    private int achievement_count;
+}

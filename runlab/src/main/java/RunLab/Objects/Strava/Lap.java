af8005c8c1873902e2c6180dd493916ae800807c
Oliver <oliver.eisenberg88@gmail.com>
2022-07-05 11:23:58 +0100

    Changed package

---
+++ runlab/src/main/java/RunLab/Objects/Strava/Lap.java
@@ -0,0 +1,17 @@
+package RunLab.Objects.Strava;
+
+
+public class Lap {
+    private Long id;
+    private String name;
+    private float distance;
+    private float elapsed_time;
+    private float split;
+    private float max_speed;
+    private float average_speed;
+    private float max_heartrate;
+    private float average_heartrate;
+    private float pace_zone;
+
+    
+}

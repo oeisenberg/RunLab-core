17e1608cb3af4c6683a0b176baae3970dd8a508c
Oliver <oliver.eisenberg88@gmail.com>
2022-07-09 14:10:14 +0100

    Updaing pathing

--- runlab/src/main/java/RunLab/EndPoints/StravaController.java
+++ runlab/src/main/java/RunLab/EndPoints/StravaController.java
@@ -17,11 +17,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
+@RequestMapping("/Strava/v1")
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class StravaController {
 
--- runlab/src/main/java/RunLab/RunlabApplication.java
+++ runlab/src/main/java/RunLab/RunlabApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RunlabApplication {
-    
+
     public static void main(String[] args) {
-        System.setProperty("server.servlet.context-path", "/Runlab");
+        System.setProperty("server.servlet.context-path", "/RunLab");
         SpringApplication.run(RunlabApplication.class, args);
     }
 

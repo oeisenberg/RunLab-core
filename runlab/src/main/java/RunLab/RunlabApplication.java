842922274b4a39eb212c99fccd187a7d4694f9c1
Oliver <oliver.eisenberg88@gmail.com>
2022-07-09 20:33:29 +0100

    rm system property

--- runlab/src/main/java/RunLab/RunlabApplication.java
+++ runlab/src/main/java/RunLab/RunlabApplication.java
@@ -2,12 +2,20 @@ package RunLab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import RunLab.Wrappers.Strava;
 
 @SpringBootApplication
 public class RunlabApplication {
 
+    @Bean
+    public Strava stravaWrapper() {
+        return new Strava();
+    }
+
     public static void main(String[] args) {
-        System.setProperty("server.servlet.context-path", "/RunLab");
+        // System.setProperty("server.servlet.context-path", "/RunLab");
         SpringApplication.run(RunlabApplication.class, args);
     }
 

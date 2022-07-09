e073d07018608ad4208ff877f5e7ccbe6f675aa1
Oliver <oliver.eisenberg88@gmail.com>
2022-07-09 14:36:05 +0100

    Adds flexibility to pass other athlete ids

--- runlab/src/main/java/RunLab/EndPoints/StravaController.java
+++ runlab/src/main/java/RunLab/EndPoints/StravaController.java
@@ -16,10 +16,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -55,10 +55,10 @@ public class StravaController {
         return r;
     }
 
-    @GetMapping(value = { "/getAthleteStatistics/{AthleteID}" })
-    public CustomResponse<AthleteStatistics> getAthleteStatistics(@PathVariable(value = "AthleteID") String AthleteID) throws InvalidRequest, IOException {
+    @GetMapping(value = { "/getAthleteStatistics" })
+    public CustomResponse<AthleteStatistics> getAthleteStatistics(@RequestParam(value = "ID", defaultValue="16443776") String ID) throws InvalidRequest, IOException {
         logger.info("Athlete Statistics Called");
-        Response response = this.stravaWrapper.getAthleteStats(AthleteID);
+        Response response = this.stravaWrapper.getAthleteStats(ID);
         Success<AthleteStatistics> r = new Success<AthleteStatistics>();
 
         AthleteStatistics stats = gson.fromJson(response.body().string().replace("\"", "'"), AthleteStatistics.class);

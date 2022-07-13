146137638967cf4132478806adff22a61c3a5bae
Oliver <oliver.eisenberg88@gmail.com>
2022-07-13 10:11:22 +0100

    User Repository with Queries

---
+++ runlab/src/main/java/RunLab/Repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package RunLab.Repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import RunLab.Objects.RunLab.User;
+
+public interface UserRepository extends MongoRepository<User, String>{
+    @Query("{userName:'?0'}")
+    public User findByUserName(String userName);
+    @Query(value="{lastName:'?0'}", fields="{'lastName' : 1, 'quantity' : 1}")
+    public List<User> findByLastName(String lastName);
+}

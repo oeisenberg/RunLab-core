488fcb2f87ce941538b53f398e9a05a56ed371fe
Oliver <oliver.eisenberg88@gmail.com>
2022-07-13 10:10:35 +0100

    User Model for MongoDB document

---
+++ runlab/src/main/java/RunLab/Objects/RunLab/User.java
@@ -0,0 +1,27 @@
+package RunLab.Objects.RunLab;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+    @Id
+    public String id; 
+
+    public String userName;
+    public String pwd;
+    public String firstName;
+    public String lastName;
+
+    public User() {}
+
+    public User(String userName, String pwd, String firstName, String lastName) {
+        this.userName = userName;
+        this.pwd = pwd;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User[id=%s, username='%s', firstname='%s', lastname='%s']", id, userName, firstName, lastName);
+    }
+}

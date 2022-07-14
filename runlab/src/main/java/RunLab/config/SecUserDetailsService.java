6b848dd19a4db23587284582b377b2bd54d10d1b
Oliver <oliver.eisenberg88@gmail.com>
2022-07-14 14:28:18 +0100

    Service is injected into the auth manager builder

---
+++ runlab/src/main/java/RunLab/config/SecUserDetailsService.java
@@ -0,0 +1,31 @@
+package RunLab.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import RunLab.Models.SecUserDetails;
+import RunLab.Models.MongoDB.User;
+import RunLab.Repositories.UserRepository;
+
+// A seperate service to be injected inside the AuthenticationManagerBuilder.
+// https://stackoverflow.com/questions/29606290/authentication-with-spring-security-spring-data-mongodb
+@Component
+public class SecUserDetailsService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }  else {
+            UserDetails details = new SecUserDetails(user);
+            return details;
+        }
+    }
+}

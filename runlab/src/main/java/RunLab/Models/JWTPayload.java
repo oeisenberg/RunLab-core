56476da906b77f3fa574369c27ad8ab5c5b515f1
Oliver <oliver.eisenberg88@gmail.com>
2022-07-14 14:32:48 +0100

    Payload class for recieving token from FE

---
+++ runlab/src/main/java/RunLab/Models/JWTPayload.java
@@ -0,0 +1,30 @@
+package RunLab.Models;
+
+import java.time.LocalDateTime;
+
+public class JWTPayload {
+    protected String username;
+    protected String userID;
+    // public T scopes;
+    protected String issuer = "http://localhost:8080/runlab-api";
+    public LocalDateTime createdDateTime = LocalDateTime.now();
+    protected LocalDateTime expiraryDateTime = LocalDateTime.now().plusDays(1); 
+
+
+    public JWTPayload(String username, String id) {
+        this.username = username;
+        this.userID = id;
+    }
+
+    public String getUserName(){
+        return this.username;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return this.createdDateTime;
+    }
+
+    public LocalDateTime getExpiraryDateTime() {
+        return this.expiraryDateTime;
+    }
+}

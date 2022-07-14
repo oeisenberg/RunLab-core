f6b2df18c564a824b56891162e7e089b25939e76
Oliver <oliver.eisenberg88@gmail.com>
2022-07-14 12:25:21 +0100

    Model for data when registering a new user

---
+++ runlab/src/main/java/RunLab/Models/RegisteringUser.java
@@ -0,0 +1,24 @@
+package RunLab.Models;
+
+public class RegisteringUser {
+    protected String userName;
+    protected String password;
+    protected String firstName;
+    protected String lastName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}

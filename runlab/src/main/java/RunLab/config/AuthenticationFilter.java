0d87fa2acf43af8f65e6581bfed662d7de7bb8a0
Oliver <oliver.eisenberg88@gmail.com>
2022-07-14 14:28:52 +0100

    Filter to check auth header for correct token

---
+++ runlab/src/main/java/RunLab/config/AuthenticationFilter.java
@@ -0,0 +1,50 @@
+package RunLab.config;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import RunLab.Models.JWTPayload;
+import RunLab.Models.MongoDB.User;
+import RunLab.Repositories.UserRepository;
+import RunLab.Utility.jwtTokenUtil;
+
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String username = null;
+        JWTPayload payload = null;
+        String authHeader = req.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer")) {
+            authHeader = authHeader.replace("Bearer ", "");
+            payload = jwtTokenUtil.decodeToPayload(authHeader);
+            username = payload.getUserName();
+        }
+        if (username != null) {
+            User user = userRepository.findByUserName(username);
+            if (jwtTokenUtil.validateToken(payload, user)){
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, Arrays.asList(new SimpleGrantedAuthority("USER")));
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+                logger.info("authenticated user " + username + ", setting security context");
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        
+        chain.doFilter(req, res);
+    }
+}

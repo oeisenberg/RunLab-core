b83f596e7fc68ba68154029e03ee2bc8034a4a8e
Oliver <oliver.eisenberg88@gmail.com>
2022-07-14 14:27:19 +0100

    SecUser class for the service to implment

---
+++ runlab/src/main/java/RunLab/Models/SecUserDetails.java
@@ -0,0 +1,53 @@
+package RunLab.Models;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import RunLab.Models.MongoDB.User;
+
+public class SecUserDetails implements UserDetails {
+    private User user;
+
+    public SecUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserName();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO: Add basic roles?
+        // https://stackoverflow.com/questions/7737602/example-of-custom-implementation-of-userdetails
+        return null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isEnabled();
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.user.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.user.isCredentialsNonExpired();
+    }
+}

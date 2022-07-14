c9384df650121f95b6d678c84b22d839ab1638ec
Oliver <oliver.eisenberg88@gmail.com>
2022-07-14 14:33:10 +0100

    New security config module

---
+++ runlab/src/main/java/RunLab/config/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package RunLab.config;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+// https://www.devglan.com/spring-security/spring-boot-jwt-auth
+// https://www.bezkoder.com/spring-boot-mongodb-login-example/
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SecUserDetailsService userDetailsService;
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationFilter authenticationTokenFilterBean() throws Exception {
+        return new AuthenticationFilter();
+    }
+
+    @Autowired
+    public void configAuthBuilder(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        builder.inMemoryAuthentication().withUser("username").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/v{\\d+}/token/*").permitAll()
+                .antMatchers("/v{\\d+}/token/*", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+
+        // .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+        // .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}

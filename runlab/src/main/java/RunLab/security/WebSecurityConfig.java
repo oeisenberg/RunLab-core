b06a3058b4f9ab873d3febf715984a7c66c62a7a
Oliver <oliver.eisenberg88@gmail.com>
2022-07-17 20:34:32 +0100

    rm unused import

--- runlab/src/main/java/RunLab/security/WebSecurityConfig.java
+++ runlab/src/main/java/RunLab/security/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package RunLab.security;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

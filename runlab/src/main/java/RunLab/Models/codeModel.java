4db27b28d936f6af0f963ea8658a84bbeaae73cf
Oliver <oliver.eisenberg88@gmail.com>
2022-07-18 18:41:55 +0100

    Should now add a enum to give a type to the code

--- runlab/src/main/java/RunLab/Models/codeModel.java
+++ runlab/src/main/java/RunLab/Models/codeModel.java
@@ -1,9 +1,16 @@
 package RunLab.models;
 
+import RunLab.models.mongoDB.APIDetails;
+
 public class codeModel {
     private String code;
+    private APIDetails.API_type api_type;
 
     public String getCode() {
         return this.code;
+    }
+
+    public APIDetails.API_type getAPIType() {
+        return this.api_type;
     }
 }

0d64a918be8e34db3b16b077f86c39610e755191
Oliver <oliver.eisenberg88@gmail.com>
2022-07-18 12:16:14 +0100

    Now a type declaration instead of util class

---
+++ runlab/src/main/java/RunLab/Models/Unix.java
@@ -0,0 +1,42 @@
+package RunLab.models;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+// Epoch is Unix at time 0 (Midnight, 1st Jan 1970 in UTC)
+// Unix is time passed since the Epoch
+public class Unix {
+
+    public long value;
+
+    public Unix(long value) {
+        this.value = value;
+    }
+
+    public long getValue() {
+        return this.value;
+    }
+
+    public LocalDateTime toDateTime() {
+        Instant instant = Instant.ofEpochSecond(this.value);
+        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
+    public boolean isGreaterThan(long otherUnixTime) {
+        return this.value > otherUnixTime;
+    }
+
+    public boolean isGreaterThanOrEquals(long otherUnixTime) {
+        return this.value >= otherUnixTime;
+    }
+
+    public boolean isLessThan(long otherUnixTime) {
+        return this.value < otherUnixTime;
+    }
+
+    public boolean isLessThanOrEquals(long otherUnixTime) {
+        return this.value <= otherUnixTime;
+    }
+
+}

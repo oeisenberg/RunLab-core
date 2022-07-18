d26f09c74eaaec1ed83fba7fa809bc8b9eea209c
Oliver <oliver.eisenberg88@gmail.com>
2022-07-18 17:15:35 +0100

    Exposing API details and prettier formatting

--- runlab/src/main/java/RunLab/Models/MongoDB/User.java
+++ runlab/src/main/java/RunLab/Models/MongoDB/User.java
@@ -1,6 +1,7 @@
 package RunLab.models.mongoDB;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -9,21 +10,21 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "user")
 public class User extends mongodbDocument {
     @Id
-    private String id; 
-    @Indexed(unique=true, background = true)
+    private String id;
+    @Indexed(unique = true, background = true)
     private String userName;
     private String password;
     private String firstName;
     private String lastName;
-    private List<thirdpartyAPIDetails> apiDetails;
+    private HashMap<APIDetails.API_type, APIDetails> apiDetails;
 
     private boolean isEnabled = true;
     private boolean isAccountNonExpired = true;
     private boolean isAccountNonLocked = true;
     private boolean isCredentialsNonExpired = true;
-    // private boolean isEnabled = true;
 
-    public User() {}
+    public User() {
+    }
 
     public User(String userName, String password, String firstName, String lastName) {
         this.userName = userName;
@@ -34,27 +35,40 @@ public class User extends mongodbDocument {
 
     @Override
     public String toString() {
-        return String.format("User[id=%s, username='%s', firstname='%s', lastname='%s']", id, userName, firstName, lastName);
+        return String.format("User[id=%s, username='%s', firstname='%s', lastname='%s']", id, userName, firstName,
+                lastName);
     }
 
-    public String getUserID(){
+    public String getUserID() {
         return this.id;
     }
 
-    public String getUserName(){
+    public String getUserName() {
         return this.userName;
     }
 
-    public String getFirstName(){
+    public String getFirstName() {
         return this.firstName;
     }
 
-    public String getLastName(){
+    public String getLastName() {
         return this.lastName;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return this.password;
+    }
+
+    public APIDetails getAPI(APIDetails.API_type api_type) {
+        return this.apiDetails.get(api_type);
+    }
+
+    public APIDetails removeAPI(APIDetails.API_type api_type) {
+        return this.apiDetails.remove(api_type);
+    }
+
+    public Collection<APIDetails> getAPIDetails() {
+        return this.apiDetails.values();
     }
 
     public boolean isEnabled() {
@@ -73,20 +87,24 @@ public class User extends mongodbDocument {
         return isCredentialsNonExpired;
     }
 
-    public void setUserName(String username){
+    public void updateAPI(APIDetails api) {
+        this.apiDetails.replace(api.getAPIType(), api);
+    }
+
+    public void setUserName(String username) {
         this.userName = username;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName){
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
+
 }

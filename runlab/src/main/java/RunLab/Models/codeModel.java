6a608dd25b6b47c770017f98179924a50a880d01
Oliver <oliver.eisenberg88@gmail.com>
2022-07-19 15:22:45 +0100

    Input cannot be enum so  string is used

--- runlab/src/main/java/RunLab/Models/codeModel.java
+++ runlab/src/main/java/RunLab/Models/codeModel.java
@@ -1,16 +1,14 @@
 package RunLab.models;
 
-import RunLab.models.mongoDB.APIDetails;
-
 public class codeModel {
-    private String code;
-    private APIDetails.API_type api_type;
+    public String code;
+    public String apiType;
 
     public String getCode() {
         return this.code;
     }
 
-    public APIDetails.API_type getAPIType() {
-        return this.api_type;
+    public String getAPIType() {
+        return this.apiType;
     }
 }

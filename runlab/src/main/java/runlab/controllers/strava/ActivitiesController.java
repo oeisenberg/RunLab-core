c0f984400906072884c289a49bb77231251590e9
Oliver <oliver.eisenberg88@gmail.com>
2022-08-03 13:10:51 +0100

    Stub for later expansion

--- runlab/src/main/java/runlab/controllers/strava/ActivitiesController.java
+++ runlab/src/main/java/runlab/controllers/strava/ActivitiesController.java
@@ -1,21 +1,6 @@
 package runlab.controllers.strava;
 
-import java.io.IOException;
-
-import okhttp3.Response;
-
-import com.google.gson.Gson;
-
-import runlab.models.exceptions.InvalidRequest;
-import runlab.models.responses.*;
-import runlab.models.strava.*;
-import runlab.wrappers.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/activites")
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class ActivitiesController {
-
-    private Gson gson = new Gson();
-    @Autowired
-    private StravaAPI stravaWrapper;
-    private Logger logger = LoggerFactory.getLogger(getClass());
 
     ActivitiesController() {}
 

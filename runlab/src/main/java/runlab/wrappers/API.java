922f9f7b2a21a48620de18c5a418e533bf55a020
Oliver <oliver.eisenberg88@gmail.com>
2022-08-03 12:57:04 +0100

    Correcting capitalisation

--- runlab/src/main/java/runlab/wrappers/API.java
+++ runlab/src/main/java/runlab/wrappers/API.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 
 import okhttp3.Response;
 
-import runlab.models.codeModel;
+import runlab.models.CodeModel;
 import runlab.models.exceptions.InvalidRequest;
 import runlab.models.mongoDB.APIDetails;
 
 public interface API {
     public APIDetails refreshAPIDetails(APIDetails api_details);
     public Response makeAPIRequest(String auth_token, String url) throws InvalidRequest;
-    public APIDetails createAPIDetails(codeModel code) throws InvalidRequest, IOException;
+    public APIDetails createAPIDetails(CodeModel code) throws InvalidRequest, IOException;
 }

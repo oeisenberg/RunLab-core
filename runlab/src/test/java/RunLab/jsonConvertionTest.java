45c8ec33f97f8cd74f3b95e61e9fc1955fa29e10
Oliver <oliver.eisenberg88@gmail.com>
2022-08-03 11:48:45 +0100

    mock testing unit for converting json

---
+++ runlab/src/test/java/RunLab/jsonConvertionTest.java
@@ -0,0 +1,13 @@
+package runlab;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit tests for RunlabApplication json conversion.
+ */
+public class jsonConvertionTest {
+
+}

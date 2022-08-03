13b25212f1e49a960a7a4876df6d6575968a3281
Oliver <oliver.eisenberg88@gmail.com>
2022-08-03 11:34:50 +0100

    rm caps & properties made public public for spring serialisation

--- runlab/src/main/java/RunLab/Models/Strava/SummaryActivity.java
+++ runlab/src/main/java/RunLab/Models/Strava/SummaryActivity.java
@@ -1,47 +1,46 @@
-package RunLab.models.strava;
+package runlab.models.strava;
 
 // Parameters used for Gson JSON serialisation.
-@SuppressWarnings("unused")
 public class SummaryActivity {
     
-    private MetaAthlete athlete_id;
-    private String name;
-    private float distance;
-    private float moving_time;
-    private float elapsed_time;
-    private float total_elevation_gain;
-    private String type; //ActivityType, https://developers.strava.com/docs/reference/#api-models-ActivityType
-    private String sport_type;
-    private long id;
-    private String start_date;
-    private String start_date_local;
-    private String timezone;
-    private float utc_offset;
-    // private ? location_city;
-    // private ? location_state;
-    private String location_country;
-    private int kudos_count;
-    private Polyline map;
-    private String gear_id;
+    public MetaAthlete athlete_id;
+    public String name;
+    public float distance;
+    public float moving_time;
+    public float elapsed_time;
+    public float total_elevation_gain;
+    public String type; //ActivityType, https://developers.strava.com/docs/reference/#api-models-ActivityType
+    public String sport_type;
+    public long id;
+    public String start_date;
+    public String start_date_local;
+    public String timezone;
+    public float utc_offset;
+    // public ? location_city;
+    // public ? location_state;
+    public String location_country;
+    public int kudos_count;
+    public Polyline map;
+    public String gear_id;
 
-    private float[] start_latlng; //LatLng,  https://developers.strava.com/docs/reference/#api-models-LatLng
-    private float[] end_latlng;
+    public float[] start_latlng; //LatLng,  https://developers.strava.com/docs/reference/#api-models-LatLng
+    public float[] end_latlng;
 
-    private float average_speed;
-    private float max_speed;
-    private float average_cadence;
-    private boolean has_heartrate;
-    private float elev_high;
-    private float elev_low;
-    private long upload_id;
-    private String upload_id_str;
-    private float suffer_score;
+    public float average_speed;
+    public float max_speed;
+    public float average_cadence;
+    public boolean has_heartrate;
+    public float elev_high;
+    public float elev_low;
+    public long upload_id;
+    public String upload_id_str;
+    public float suffer_score;
 
     // missing from generic call - maybe in detailed?
-        // private float calories;
-        // private float perceived_exertion;
-        // private Split[] splits;
-        // private Lap[] laps;
-        // private float type;
+        // public float calories;
+        // public float perceived_exertion;
+        // public Split[] splits;
+        // public Lap[] laps;
+        // public float type;
 
 }
